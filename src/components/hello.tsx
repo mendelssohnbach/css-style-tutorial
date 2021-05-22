912e9db0d1c8d804ad5d9bc1161e7d73cc945a40
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-23 00:54:59 +0900

    変更 CSSインラインスタイル設定

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
 
+// JSX要素用のスタイル定義（React.CSSProperties形式）
+const style: React.CSSProperties = {
+  color: 'blue',
+  background: '#aaccff',
+  padding: '0.5em',
+  fontWeight: 'bolder',
+  borderRadius: '0.5em',
+};
+
 export const Hello: React.FC = () => {
-  return <div className="hello">Hello</div>;
+  return <div style={style}>Hello</div>;
 };
