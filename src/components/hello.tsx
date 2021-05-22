6fa4d25dd7699204a965c9ebd0791c1dfe842e93
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-23 01:39:30 +0900

    修正 IProps型定義

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import './styles.css';
 
+interface IProps {
+  text: string;
+}
+
 export const Hello: React.FC<IProps> = (prop) => {
   return <div className="hello">{prop.text}</div>;
 };
