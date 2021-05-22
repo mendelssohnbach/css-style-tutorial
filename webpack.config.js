51fc680dc170e6228a762d60633d810b8f5a4bb2
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-23 01:45:48 +0900

    修正 webpackバンドラーCSSモジュール対応

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 module.exports = {
   module: {
     rules: [
+      // CSS Modules
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+            },
+          },
+        ],
       },
     ],
   },
