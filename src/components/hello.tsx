147b205eceff2adbe2fe55970fb820d0ce3c80bd
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-23 02:11:42 +0900

    応急処置:削除　スタイル読み込みと適用を削除

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import css from './hello.css';
 
 export const Hello: React.FC = () => {
-  return <div className={css.box}>Hello</div>;
+  return <div>Hello</div>;
 };
